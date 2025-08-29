5c452cafce8000ae1b42ffb3d1d8d75ddcc683cf
Benjamin Martin <132010514+benwmar@users.noreply.github.com>
2025-08-29 10:30:42 -0400

    Update getCohortsFromJHMAtlas.R
    Added atlas-demo cohort IDs

--- getCohortsFromJHMAtlas.R
+++ getCohortsFromJHMAtlas.R
@@ -3,12 +3,12 @@
 token <- ""
 
 ## Authorize WebApi
-baseUrl <- "https://atlas-de-id.pm.jh.edu/WebAPI"
+baseUrl <- "https://atlas-demo.ohdsi.org/WebAPI"
 setAuthHeader(baseUrl, authHeader = token)
 getCdmSources(baseUrl)
 
 ## Get cohort definitions via WebAPI
-cohortIds <- c(###,###)
+cohortIds <- c(1794207,1794206,1794205,1794204,1794203,1794199,1794067,1794065,1794063,1794062,1794201,1794200,1794061,1794064,1794066,1794056,1794053,1794059,1794054,1794055)
 cohortDefinitionSet <- ROhdsiWebApi::exportCohortDefinitionSet(
   baseUrl = baseUrl,
   cohortIds = cohortIds,
@@ -26,6 +26,7 @@ write(cohortDefinitionSet$json[2], file = here::here("json/###.json"))
 
 write(cohortDefinitionSet$sql[1], file = here::here("sql/sql_server/###.sql"))
 write(cohortDefinitionSet$sql[2], file = here::here("sql/sql_server/###.sql"))
+
 
 
 
